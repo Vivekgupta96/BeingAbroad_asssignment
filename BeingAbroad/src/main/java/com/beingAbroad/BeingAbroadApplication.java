05d6ccec49f4d436a77ad3c78343f0459df0012f
newExtendedTech <vivekg0691@gmail.com>
2024-02-22 06:59:04 +0530

    assignment

---
+++ BeingAbroad/src/main/java/com/beingAbroad/BeingAbroadApplication.java
@@ -0,0 +1,13 @@
+package com.beingAbroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeingAbroadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeingAbroadApplication.class, args);
+	}
+
+}
